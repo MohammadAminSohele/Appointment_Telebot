612505848f7298aaad3c6b9f219d899db58c9a52
MohammadAminSohele <mohammadaminsohele@gmail.com>
2026-01-04 18:25:53 +0330

    initial commit

---
+++ query.py
@@ -0,0 +1,116 @@
+import psycopg2
+from schema import db_url
+
+def connect_to_db():
+    conn = psycopg2.connect(db_url)
+    cursor = conn.cursor()
+    return conn, cursor
+
+def insert_user(user_id, username):
+    conn ,cursor = connect()
+
+    cursor.execute("INSERT OR IGNORE INTO users (user_id, username) VALUES (%s, %s)", (user_id, username))
+
+    conn.commit()
+    conn.close()
+
+def get_services():
+    conn, cursor = connect()
+
+    cursor.execute("SELECT service_id, name FROM services")
+    services = cursor.fetchall()
+
+    conn.close()
+    return services
+
+def get_dates(service_id):
+    conn, cursor  = connect()
+
+    cursor.execute("SELECT DISTINCT date FROM slots WHERE service_id = %s", (service_id,))
+    dates = [row[0] for row in cursor.fetchall()]
+
+    conn.close()
+    return dates
+
+def get_times(service_id, date):
+    conn, cursor  = connect()
+
+    cursor.execute("""
+        SELECT slot_id, time FROM slots
+        WHERE service_id = %s AND date = %s AND status = 'available'
+        ORDER BY time ASC
+    """, (service_id, date))
+    times = cursor.fetchall()
+
+    conn.close()
+    return times
+
+def book_appointments(user_id,slot_id):
+    conn,cursor = connect()
+
+    cursor.execute("INSERT INTO appointments (user_id,slot_id) VALUES(%s,%s)",(user_id,slot_id))
+
+    conn.commit()
+    conn.close()
+
+def update_slots_status(slot_id):
+    conn,cursor = connect()
+
+    cursor.execute("UPDATE slots SET status = 'booked' WHERE slot_id=%s ",(slot_id,))
+
+    conn.commit()
+    conn.close()
+
+def get_user_appointments(user_id):
+    conn,cursor = connect()
+
+    cursor.execute(
+        """ 
+        SELECT s.date,s.time,sv.name
+        FROM appointments a
+        JOIN slots s ON a.slot_id=s.slot_id
+        JOIN services sv ON sv.service_id=s.service_id
+        WHERE a.user_id=%s
+        ORDER BY s.date,s.time
+        """,(user_id,)
+    )
+    result = cursor.fetchall()
+    conn.close()
+    return result
+
+def get_admin_appointments(admin_id):
+    conn,cursor = connect()
+
+    cursor.execute(
+        """ 
+        SELECT s.date,s.time,sv.name,u.username
+        FROM appointments a
+        JOIN slots s ON s.slot_id = a.slot_id
+        JOIN services sv ON s.service_id = sv.service_id
+        JOIN users u ON u.user_id = a.user_id
+        WHERE sv.admin_id = %s
+        ORDER BY s.date,s.time
+        """,(admin_id,)
+    )
+    result = cursor.fetchall()
+    conn.close()
+    return result
+
+def insert_slots(service_id,date,times):
+    conn,cursor = connect()
+
+    for time in times:
+        cursor.execute("INSERT INTO slots (service_id,date,time,status) VALUES (%s,%s,%s,'available')",(service_id,date,time))
+
+    conn.commit()
+    conn.close()
+
+def insert_service(name,admin_id):
+    conn,cursor = connect()
+
+    cursor.execute("INSERT INTO services (name,admin_id) VALUES (%s,%s)",(name,admin_id))
+    service_id = cursor.lastrowid
+
+    conn.commit()
+    conn.close()
+    return service_id
---
+++ schema.py
@@ -0,0 +1,52 @@
+import psycopg2
+import os 
+from dotenv import load_dotenv 
+
+load_dotenv()
+
+db_url = os.getenv('DATABASE_URL') 
+
+def create_tables():
+    conn = psycopg2.connect(db_url)
+    cur = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        user_id TEXT PRIMARY KEY,
+        username TEXT
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS services (
+        service_id SERIAL PRIMARY KEY,
+        name TEXT,
+        admin_id TEXT
+    )
+    """)
+
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS slots (
+        slot_id SERIAL PRIMARY KEY,
+        service_id INTEGER,
+        date TEXT,
+        time TEXT,
+        status TEXT DEFAULT 'available'
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS appointments (
+        appointment_id SERIAL PRIMARY KEY,
+        user_id TEXT,
+        slot_id INTEGER
+    )
+    """)
+
+    conn.commit()
+    conn.close()
+
+if __name__ == "__main__":
+    create_tables()
+    print("Tables created successfully!")
